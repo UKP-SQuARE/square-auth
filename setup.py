26650040ee0b2ce2336b54c84df6486a567dfb98
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-02-21 21:10:48 +0100

    exclude gitgnore and dev reqs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="www.informatik.tu-darmstadt.de/ukp",
     author="UKP",
     author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
-    packages=find_packages(exclude=("tests",)),
+    packages=find_packages(exclude=("tests", ".gitignore", "requirements.dev.txt")),
     install_requires=[
         "pyjwt[crypto]>=2.3.0",
         "requests>=2.26.0",
