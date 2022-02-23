5a0647539fbcf015f88230fccfe95edd309ae16d
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-02-23 08:47:01 +0100

    make realm and keycloak_base_url optional

--- square_auth/client_credentials.py
+++ square_auth/client_credentials.py
@@ -8,8 +8,8 @@ from square_auth.keycloak_api import KeycloakAPI
 class ClientCredentials:
     def __init__(
         self,
-        keycloak_base_url: str,
-        realm: str,
+        keycloak_base_url: str = None,
+        realm: str = None,
         client_id: str = None,
         client_secret: str = None,
         buffer: int = 60,
@@ -17,19 +17,52 @@ class ClientCredentials:
         """Obtains Access Tokens via Client Credentials flow.
 
         Args:
-            keycloak_base_url (str): URL of the keycloak instance.
-            realm (str): Realm of the expected tokens.
-            client_id (str, optional): The Client ID used for requesting access tokens. If not provided will attempt to read from `CLIENT_ID` environment variable. Defaults to None.
-            client_secret (str, optional): The Client Secret used for requesting access tokens. If not provided will attempt to read from `CLIENT_SECERT` environment variable.. Defaults to None.
+            realm (str, optional): Realm of the expected tokens. If not set, will attempt to read from `REALM` environment variable. Defaults to None.
+            keycloak_base_url (str, optional): URL of the keycloak instance, if not set, will attempt to read from `KEYCLOAK_BASE_URL` environment variable. Defaults to None.
+            client_id (str, optional): The Client ID used for requesting access tokens. If not set, will attempt to read from `CLIENT_ID` environment variable. Defaults to None.
+            client_secret (str, optional): The Client Secret used for requesting access tokens. If not set, will attempt to read from `CLIENT_SECERT` environment variable. Defaults to None.
             buffer (int, optional): Returned tokens are at least `buffer` seconds valid. Defaults to 60.
         """
-        self.keycloak_api = KeycloakAPI(keycloak_base_url)
+        self.keycloak_base_url = keycloak_base_url
         self.realm = realm
         self.client_id = client_id
         self.client_secret = client_secret
         self.buffer = buffer
 
+        self.keycloak_api = KeycloakAPI(keycloak_base_url)
         self.token = self.renew_token()
+
+    @property
+    def keycloak_base_url(self):
+        return self._keycloak_base_url
+
+    @keycloak_base_url.setter
+    def keycloak_base_url(self, value):
+
+        if value is None:
+            value = os.getenv("KEYCLOAK_BASE_URL", None)
+            if value is None:
+                raise ValueError(
+                    "Either provide keycloak_base_url as parameter or set "
+                    "KEYCLOAK_BASE_URL environment variable."
+                )
+        self._keycloak_base_url = value
+    
+    @property
+    def realm(self):
+        return self._realm
+
+    @realm.setter
+    def realm(self, value):
+
+        if value is None:
+            value = os.getenv("REALM", None)
+            if value is None:
+                raise ValueError(
+                    "Either provide realm as parameter or set REALM environment "
+                    "variable."
+                )
+        self._realm = value
 
     @property
     def client_id(self):
